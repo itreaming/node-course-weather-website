fc51fba11c328dc1513b19a9e30a0da024250b99
itreaming <grigoryeva.darya@gmail.com>
2023-09-13 13:07:40 +0200

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#messageLocation')
+const messageTwo = document.querySelector('#messageForecast')
+const messageThree = document.querySelector('#messageError')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    messageOne.textContent = 'Loading'
+    messageTwo.textContent = ''
+    messageThree.textContent = ''
+
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageThree.textContent = data.error
+                messageOne.textContent = ''
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+
+})
